a5ebc1c2cf72b42ddc3de2454c0053638b804090
eliasjuk <eliasp.juk@hotmail.com>
2024-10-12 20:27:05 -0300

    funcao limpar console

---
+++ src/utils/ConsoleClear.java
@@ -0,0 +1,16 @@
+package utils;
+
+public class ConsoleClear {
+    // Método para limpar o console
+    public static void clearConsole() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao tentar limpar o console.");
+        }
+    }    
+}
