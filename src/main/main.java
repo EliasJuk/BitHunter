88cefbd249eac1236f8b069b380bef2f6e919ceb
eliasjuk <eliasp.juk@hotmail.com>
2024-11-02 11:05:20 -0300

    chamar funções

--- src/main/main.java
+++ src/main/main.java
@@ -6,9 +6,6 @@ import java.util.Scanner;
 import utils.ColorPalette;
 import utils.ConsoleClear;
 
-import handlers.EllipticTools;
-import encoding.WIFGenerator;
-
 public class main {
 	public static void main(String args[]) throws Exception, NoSuchAlgorithmException{        
 		while(true){
@@ -16,44 +13,37 @@ public class main {
 			
 			System.out.println(ColorPalette.CYAN + "╔═══════════════════════════════╗" + ColorPalette.RESET);
 			System.out.println(ColorPalette.CYAN + "║                               ║" + ColorPalette.RESET);
-			System.out.println(ColorPalette.CYAN + "║"+ColorPalette.PINK+"       SatoshiToolBox Menu     "+ColorPalette.CYAN+"║" + ColorPalette.RESET);
+			System.out.println(ColorPalette.CYAN + "║"+ColorPalette.PINK+"          BIT HUNTER           "+ColorPalette.CYAN+"║" + ColorPalette.RESET);
 			System.out.println(ColorPalette.CYAN + "║"+ColorPalette.YELLOW+"             v0.1              "+ColorPalette.CYAN+"║" + ColorPalette.RESET);
 			System.out.println(ColorPalette.CYAN + "║                               ║" + ColorPalette.RESET);
 			System.out.println(ColorPalette.CYAN + "╚═══════════════════════════════╝" + ColorPalette.RESET);
-			System.out.println(ColorPalette.YELLOW + "Choose an option:" + ColorPalette.RESET);
+			System.out.print("");
 			System.out.println(
+					"-------------------------------------------\n"+
 					" 1 - CONVERT PRIVATE KEY TO WIF\n" +
 					" 2 - GENERATE PUBLIC KEY\n" +
 					" 3 - GENERATE BTC ADDRESS\n" +
+					"-------------------------------------------\n"+
 					" 4 - EllipticTools\n" +
 					" 5 - RIPDM168 COMPARE\n" +
-					" 0 - EXIT");
+					"-------------------------------------------\n"+
+					" 0 - EXIT\n");
 
+			System.out.print(ColorPalette.YELLOW + "Choose an option: " + ColorPalette.RESET);
 			Scanner scanner = new Scanner(System.in);
 			int scannerMenu = scanner.nextInt();
 
 			switch (scannerMenu) {
 				case 1:
-					ConsoleClear.clearConsole();
-					System.out.println("Converting private key to WIF...\n");
-					
-					System.out.print("INFORME SUA PRIVATE KEY: ");                    
-					Scanner scanner2 = new Scanner(System.in);
-					String privateKeyHex = scanner2.nextLine();
-					
-					System.out.println(ColorPalette.PINK+"\nWIF: "+WIFGenerator.KeyToWIF(privateKeyHex, true));
-					
-					Scanner pause = new Scanner(System.in);
-					pause.nextLine(); // Aguarda o usuário pressionar Enter
+					callFunctions.wifGenerate();
 					break;
 				case 2:
-					System.out.println("Generating public key...");
+					callFunctions.publicKeyGenerate();
 					break;
 				case 3:
-					System.out.println("Generating Bitcoin address...");
+					callFunctions.addressGenerate();
 					break;
 				case 4:
-					generatePublicKey("aca5882db643df97cc4b39608b258f7be8b53b6f38a2c1f04b1e77b180a872bb");
 					break;
 				case 0:
 					System.out.println("Exiting...");
@@ -66,15 +56,4 @@ public class main {
 			}
 		}
 	}
-
-	public static void generatePublicKey(String privateKeyHex) throws NoSuchAlgorithmException, Exception{        
-    //GERA A CHAVE PUBLICA APARTIR DA CHAVE PRIVADA (COMPRIMIDA)
-    EllipticTools elpt = new EllipticTools();
-    String compressedPublicKey = elpt.main(privateKeyHex);
-    System.err.println("CHAVE PUBLICA COMPRIMIDA: " + compressedPublicKey);
-
-		//PAUSE
-		Scanner scanner = new Scanner(System.in);
-		String objScanner = scanner.next();
-  }
 }
