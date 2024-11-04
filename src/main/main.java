7634c35d3f460761dfe43eec009d57f08fca96d8
eliasjuk <eliasp.juk@hotmail.com>
2024-11-04 19:28:41 -0300

    add opções ao main menu

--- src/main/main.java
+++ src/main/main.java
@@ -3,33 +3,38 @@ package main;
 import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
+import main.functions.btc.BtcFinder;
+import main.functions.btc.BtcPuzzle;
 import utils.ColorPalette;
 import utils.ConsoleClear;
 
 public class main {
-	public static void main(String args[]) throws Exception, NoSuchAlgorithmException{        
-		while(true){
+	public static void main(String args[]) throws Exception, NoSuchAlgorithmException {
+		while (true) {
 			ConsoleClear.clearConsole();
-			
+
 			System.out.println(ColorPalette.CYAN + "╔═══════════════════════════════╗" + ColorPalette.RESET);
 			System.out.println(ColorPalette.CYAN + "║                               ║" + ColorPalette.RESET);
-			System.out.println(ColorPalette.CYAN + "║"+ColorPalette.PINK+"          BIT HUNTER           "+ColorPalette.CYAN+"║" + ColorPalette.RESET);
-			System.out.println(ColorPalette.CYAN + "║"+ColorPalette.YELLOW+"             v0.1              "+ColorPalette.CYAN+"║" + ColorPalette.RESET);
+			System.out.println(ColorPalette.CYAN + "║" + ColorPalette.PINK + "          BIT HUNTER           "
+					+ ColorPalette.CYAN + "║" + ColorPalette.RESET);
+			System.out.println(ColorPalette.CYAN + "║" + ColorPalette.YELLOW + "             v0.1              "
+					+ ColorPalette.CYAN + "║" + ColorPalette.RESET);
 			System.out.println(ColorPalette.CYAN + "║                               ║" + ColorPalette.RESET);
 			System.out.println(ColorPalette.CYAN + "╚═══════════════════════════════╝" + ColorPalette.RESET);
 			System.out.print("");
 			System.out.println(
-					"-------------------------------------------\n"+
-					" 1 - CONVERT PRIVATE KEY TO WIF\n" +
-					" 2 - GENERATE PUBLIC KEY\n" +
-					" 3 - GENERATE BTC ADDRESS\n" +
-					"-------------------------------------------\n"+
-					" 4 - Only Target Wallet\n" +
-					" 5 - RIPDM168 COMPARE\n" +
-					"-------------------------------------------\n"+
-					" 9 - Wallet Balance\n" +
-					"-------------------------------------------\n"+
-					" 0 - EXIT\n");
+					"-------------------------------------------\n" +
+							" 1 - CONVERT PRIVATE KEY TO WIF\n" +
+							" 2 - GENERATE PUBLIC KEY\n" +
+							" 3 - GENERATE BTC ADDRESS\n" +
+							"-------------------------------------------\n" +
+							" 4 - Target Wallet Address\n" +
+							" 5 - Puzzle Wallet\n" +
+							" 6 - RIPDM168 COMPARE\n" +
+							"-------------------------------------------\n" +
+							" 9 - Wallet Balance\n" +
+							"-------------------------------------------\n" +
+							" 0 - EXIT\n");
 
 			System.out.print(ColorPalette.YELLOW + "Choose an option: " + ColorPalette.RESET);
 			Scanner scanner = new Scanner(System.in);
@@ -46,6 +51,12 @@ public class main {
 					callFunctions.addressGenerate();
 					break;
 				case 4:
+					BtcFinder btcfinder = new BtcFinder();
+					btcfinder.findKeyInRange();
+					break;
+				case 5:
+					BtcPuzzle btcpuzzle = new BtcPuzzle();
+					btcpuzzle.findKeyInRange();
 					break;
 				case 9:
 					callFunctions.getBalance();
