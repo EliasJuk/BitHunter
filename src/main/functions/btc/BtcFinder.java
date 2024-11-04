22bb7b6ca63413230e0a68bcd761ea8e5b9f0c8b
eliasjuk <eliasp.juk@hotmail.com>
2024-11-04 18:18:47 -0300

    refatorado

--- src/main/functions/btc/BtcFinder.java
+++ src/main/functions/btc/BtcFinder.java
@@ -14,48 +14,52 @@ import utils.KeyDataLogger;
  */
 
 public class BtcFinder {
-  private rangeData puzzle;
+  private dataPuzzle puzzle;
 
   public BtcFinder() {
-    this.puzzle = new rangeData();
-    puzzle.Puzzle(); // Inicializa os intervalos do puzzle
+    this.puzzle = new dataPuzzle();
   }
 
-  // Método para encontrar a chave no intervalo especificado pelo usuário.
-  public void findKeyInRange() {
-    try (Scanner scanner = new Scanner(System.in)) {
-      System.out.print("Digite o número do desafio (1:120): ");
+public void findKeyInRange() {
+  try (Scanner scanner = new Scanner(System.in)) {
+      System.out.print("Digite o número de Bits da carteira alvo (1:160): ");
       int challengeNumber = scanner.nextInt();
       challengeNumber--; // Decrementa para usar como índice
 
       // Verifica se o desafio existe
       if (challengeNumber < 0 || challengeNumber >= puzzle.ranges.size()) {
-        System.out.println("Desafio inválido.");
-        return;
+          System.out.println("Desafio inválido.");
+          return;
       }
 
       // Obtém os intervalos de chaves para o desafio selecionado
-      BigInteger startIndex = puzzle.ranges.get(challengeNumber).getStartHex();
-      BigInteger endIndex = puzzle.ranges.get(challengeNumber).getEndHex();
+      PuzzleClass selectedRange = puzzle.ranges.get(challengeNumber);
+      BigInteger startIndex = selectedRange.getStartHex();
+      BigInteger endIndex = selectedRange.getEndHex();
 
-      System.out.println("\nIntervalo de chaves: " + startIndex + " até " + endIndex);
-
-      System.out.print("Digite o endereço Bitcoin para comparar: ");
+      // Pergunta ao usuário o endereço Bitcoin para comparar
+      System.out.print("\nDigite o endereço Bitcoin para comparar: ");
       String targetAddress = scanner.next();
+
+      // Calcula o total de combinações
+      BigInteger totalCombinacoes = endIndex.subtract(startIndex).add(BigInteger.ONE);
+      
+      System.out.println("Total de combinações possíveis: " + totalCombinacoes);
+      System.out.println("\nRanges:");
+      System.out.println("DECIMAL:     " + startIndex + " até " + endIndex);
+      System.out.println("HEXADECIMAL: " + startIndex.toString(16) + " até " + endIndex.toString(16));
 
       // Chama o método para gerar chaves e comparar
       addressComparte(startIndex, endIndex, targetAddress, challengeNumber);
-    } catch (Exception e) {
+  } catch (Exception e) {
       System.out.println("Ocorreu um erro: " + e.getMessage());
       e.printStackTrace();
-    }
   }
+}
 
-
-  // Método para comparar endereços gerados com o endereço alvo dentro de um intervalo específico.
+  // Método para comparar endereços gerados com o endereço alvo
   public static void addressComparte(BigInteger startIndex, BigInteger endIndex, String targetAddress,
-      int challengeNumber)
-      throws Exception {
+      int challengeNumber) throws Exception {
     SecureRandom random = new SecureRandom();
     addressGenerator addrGen = new addressGenerator();
 
@@ -66,11 +70,11 @@ public class BtcFinder {
     // Gera chaves até que o endereço correspondente seja encontrado
     while (!targetAddress.equals(bitcoinAddress)) {
       count++;
-      
+
       // Gera uma chave aleatória dentro do intervalo
       BigInteger hexRandomKey = startIndex.add(new BigInteger(endIndex.subtract(startIndex).bitLength(), random));
 
-      // Converte o valor randomico para hexadecimal
+      // Converte o valor aleatório para hexadecimal
       privateKeyHex = hexRandomKey.toString(16);
 
       // Gera a chave pública e o endereço
@@ -78,24 +82,20 @@ public class BtcFinder {
       bitcoinAddress = addrGen.generate(compressedPublicKey);
 
       // Imprime informações das chaves geradas e tentativas
-      System.out.println("\nPrivate: " + privateKeyHex);
-      System.out.println("Public: " + compressedPublicKey);
-      System.out.println("Address: " + bitcoinAddress);
-      System.out.println("Attempts: " + count);
+      System.out.printf("\rPrivate: %s | Public: %s | Address: %s | Attempts: %d",
+          privateKeyHex, compressedPublicKey, bitcoinAddress, count);
 
-      // Verifica se o endereço gerado corresponde ao alvo
-      if (targetAddress.equals(bitcoinAddress)) {
-        break; // Sai do loop se os endereços forem iguais
-      }
+      System.out.flush();
     }
-    
-    System.out.println("Win: " + privateKeyHex);
 
-    // Preenche a privateKeyHex com zeros à esquerda se necessário para ter 64 caracteres
+    System.out.println("\nWin: " + privateKeyHex);
+
+    // Preenche a privateKeyHex com zeros à esquerda - 64 caracteres
     privateKeyHex = String.format("%064x", new BigInteger(privateKeyHex, 16));
 
     // Quando os endereços forem iguais, armazena em um arquivo TXT
-    KeyDataLogger.writeToFile("FoundKeys.txt", "Challenge: " + (challengeNumber+1) + " - Attempts: " + count + " - PrivateKey: " + privateKeyHex);
+    KeyDataLogger.writeToFile("FoundKeys.txt",
+        "Challenge: " + (challengeNumber + 1) + " - Attempts: " + count + " - PrivateKey: " + privateKeyHex);
   }
 
   public static void main(String[] args) {
